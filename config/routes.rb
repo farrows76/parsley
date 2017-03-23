192dec6f8eba12ffe59cda168f74b649832d7f16
Steve Farrow <farrows76@gmail.com>
2017-03-23 08:33:19 -0700

    Added the file upload route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
   put '/update', to: 'items#update', as: :item_update
   post '/create', to: 'items#create', as: :item_create
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  post '/upload', to: 'items#upload', as: :upload_file
 end
