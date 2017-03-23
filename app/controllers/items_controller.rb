dfef74faaa2eb628dfa01933020b152e86e54de6
Steve Farrow <farrows76@gmail.com>
2017-03-23 08:35:36 -0700

    Added the file upload method

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,17 +2,22 @@ class ItemsController < ApplicationController
 
   def update
     item = Item.find(params[:id])
-    if item && item.update(params)
+    if item && item.update(params[:item])
       render status: :ok, json: item
     end
   end
 
   def create
-    item = Item.new(params)
+    item = Item.new(params[:item])
     if item.create
       render status: :ok, json: item
     else
       
     end
+  end
+
+  def upload
+    url = params[:url]
+    results = ParserService.new(url).upload_file
   end
 end
