e18ac0d94dc0134e80222110c515bd155dda0136
Steve Farrow <farrows76@gmail.com>
2017-03-24 11:09:30 -0700

    added default json to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get '/find/:id', to: 'items#find', as: :find_item
-  put '/update', to: 'items#update', as: :item_update
-  post '/create', to: 'items#create', as: :item_create
-  post '/upload', to: 'items#upload', as: :upload_file
+  get '/find/:id', to: 'items#find', defaults: { format: :json }, as: :find_item
+  put '/update', to: 'items#update', defaults: { format: :json }, as: :item_update
+  post '/create', to: 'items#create', defaults: { format: :json }, as: :item_create
+  post '/upload', to: 'items#upload', defaults: { format: :json }, as: :upload_file
 end
